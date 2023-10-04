1867d27cdaad8e8bc00ed6c821f9c7a74ba1e583
Dango <7150macNV777DAI@gmail.com>
2023-10-04 17:45:59 +0900

    update: directory structure

--- build.ps1
+++ build.ps1
@@ -1,13 +1,13 @@
 # venvをアクティベート
-. .\venv\Scripts\Activate
+. .\Middleware\venv\Scripts\Activate
 
 # requirements.txtを生成
-pip freeze | Out-File -Encoding utf8 requirements.txt
+pip freeze | Out-File -Encoding utf8 .\Middleware\requirements.txt
 
 # venvをディアクティベート
 deactivate
 
 # Dockerイメージをビルド
-docker build -t mr-middleware-ws .
+docker build -t mr-middleware-ws .\Middleware\
 
 Write-Output "Docker build complete!"
