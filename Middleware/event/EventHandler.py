e1e60a1eb2bf312de475bd55e66b0f3fed5df8b4
Dango <7150macNV777DAI@gmail.com>
2023-10-05 17:11:02 +0900

    add: position sync

--- Middleware/event/EventHandler.py
+++ Middleware/event/EventHandler.py
@@ -3,7 +3,7 @@ from ..Auguma import TopicHanlder, ClientManager
 
 
 @TopicHanlder.topic_handler("event")
-async def handle_event(content: dict, client_man: ClientManager):
+async def handle_event(idx: str, content: dict, client_man: ClientManager):
     event_name = content["name"]
     await client_man.send_dt(json.dumps({
         "topic": "event",
@@ -12,3 +12,14 @@ async def handle_event(content: dict, client_man: ClientManager):
         }
     }))
 
+
+@TopicHanlder.topic_handler("position")
+async def handle_position(idx: str, content: dict, client_man: ClientManager):
+    await client_man.send_dt(json.dumps({
+        "topic": "position",
+        "content": {
+            "cid": idx,
+            "position": content["position"],
+            "rotation": content["rotation"]
+        }
+    }))
